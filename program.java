407deccc1e9a76cc56373ba3a2f38cca1d3d20ab
raphischillmeier <142599185+raphischillmeier@users.noreply.github.com>
2024-01-17 07:43:51 +0100

    start

---
+++ program.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class program{
+
+    public static void main(String[] args){
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter Username");
+        String username = myScanner.nextLine();
+        System.out.println(username);
+
+        
+    }
+
+}
