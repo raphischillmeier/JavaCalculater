b0adeda880f421bebed864a4a884f74001ad11cb
raphischillmeier <schillmeierraphael@gmail.com>
2024-01-17 08:58:12 +0100

    finished

--- program.java
+++ program.java
@@ -1,16 +1,53 @@
 import java.util.Scanner;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class program{
 
     public static void main(String[] args){
 
+        String calcString = askCalculation();
+
+        Object resultCalc = eval(calcString);
+        
+        System.out.println(resultCalc);
+    }
+
+    //method for asking about the calculation input
+    public static String askCalculation(){
+
+        //defines a scanner for user input
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("Enter Username");
-        String username = myScanner.nextLine();
-        System.out.println(username);
+        //Asks for the caclulation
+        System.out.println("Gib deine Rechnung ein:");
+        String calc = myScanner.nextLine();
 
-        
+        //returns calc 
+        return calc;
+
+    }
+
+    public static Object eval(String calc) {
+        try {
+            // Klasse von javax.script, ermöglicht das Auffinden und Erstellen von ScriptEngine-Instanzen
+            ScriptEngineManager manager = new ScriptEngineManager();
+
+            // Hier wird eine Instanz der ScriptEngine für JavaScript angefordert
+            ScriptEngine engine = manager.getEngineByName("js");
+
+            // Die eval-Methode gibt normalerweise ein Object zurück
+            Object result = engine.eval(calc);
+
+            return result;
+        } catch (ScriptException e) {
+            // Fehler beim Auswerten der Rechnung
+            e.printStackTrace();
+            return null; // Oder handle den Fehler entsprechend
+        }
     }
 
 }
+    
+
