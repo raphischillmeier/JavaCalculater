8ea0e166745eadd24e2c64c0ec35ee8308463185
raphischillmeier <142599185+raphischillmeier@users.noreply.github.com>
2024-01-18 14:35:09 +0100

    first steps

---
+++ guiTaschenrechner/Fenster.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class Fenster extends JFrame {
+    
+    public Fenster{
+
+    setLayout(null);
+
+    setVisible(true);
+
+    setSize(400, 400);
+
+    setTitle("Taschenrechner");
+
+    setDefaultCloseOperation(JFrame.EXIT_ON_Close);
+
+    }
+
+}
---
+++ guiTaschenrechner/program.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class program{
+
+    public static void main(String[] args){
+
+    }
+
+}
