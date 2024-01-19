c22e3be09b548ea4690faa035943150f59519e77
raphischillmeier <142599185+raphischillmeier@users.noreply.github.com>
2024-01-19 10:28:02 +0100

    added labels

--- guiTaschenrechner/Fenster.java
+++ guiTaschenrechner/Fenster.java
@@ -1,9 +1,14 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class Fenster extends JFrame {
     
+    private JButton buttonEvaluate;
+    private JTextField calculation;
+    private JTextField answer;
+
     public Fenster(){
 
     setLayout(null);
@@ -15,6 +20,17 @@ public class Fenster extends JFrame {
     setTitle("Taschenrechner");
 
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    buttonEvaluate = new JButton("Ausrechnen:");
+    calculation = new JTextField(15);
+    answer = new JTextField(15);
+    answer.setEditable(false);
+
+    add(buttonEvaluate);
+    add(calculation);
+    add(answer);
+
 
     }
 
